d8e8a5dcd426dd3c82e9e162731a966133dee030
Aaron Smith <aaron@smartpayrentals.com>
2021-08-30 20:52:42 -0400

    Add landing page with test class

---
+++ symfony/src/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+	
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function home(): Response
+	{
+		
+		
+		return $this->render(
+			'index.html.twig',
+			[
+				
+			]
+		);
+	}
+	
+	
+}
