97148c13248f7fa3ce21b8820be12be8c06d654c
Aaron Smith <aaron@smartpayrentals.com>
2021-08-31 15:54:04 -0400

    Add loan results to page

--- symfony/src/Controller/DefaultController.php
+++ symfony/src/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\LoanParameter;
+use App\Service\LoanCalculator;
 
 /**
  * @Route("/")
@@ -17,23 +19,51 @@ class DefaultController extends AbstractController
 	/**
 	 * @Route("/", name="home")
 	 */
-	public function home(Request $request): Response
+	public function home(Request $request, LoanParameter $loanParameterService, LoanCalculator $loanCalculatorService): Response
 	{
 		$form = $this->createForm(LoanType::class);
+		
+		$formErrors = [];
+		$loanData = [];
 		
 		$form->handleRequest($request);
 		if ($form->isSubmitted() && $form->isValid()) {
 			$data = $form->getData();
 			
 			//  Process submitted data
+			try {
+				//  Check for requesting too large of an amount
+				if ($data['amount'] > $loanParameterService->getMaxAmount($data['creditScore'])) {
+					$formErrors[] = 'The maximum loan amount for your credit score is: $'.$loanParameterService->getMaxAmount($data['creditScore']);
+				}
+			} catch (\Exception $e) {
+				$formErrors = ['Please check your inputs and resubmit the form'];
+			}
 			
 			
+			if (empty($formErrors)) {
+				try {
+					$interestRate = $loanParameterService->getInterestRate($data['term'], $data['creditScore']);
+					
+					$fee = $loanParameterService->getOriginationFee($data['amount']);
+					
+					$payment = $loanCalculatorService->getMonthlyPayment($data['amount'] + $fee, $interestRate, $data['term']);
+					
+					$loanData['interestRate'] = $interestRate;
+					$loanData['fee'] = $fee;
+					$loanData['payment'] = $payment;
+				} catch (\Exception $e) {
+					$formErrors = ['Please check your inputs and resubmit the form'];
+				}
+			}
 		}
 		
 		return $this->render(
 			'index.html.twig',
 			[
-				'form' => $form->createView(),
+				'form'       => $form->createView(),
+				'formErrors' => $formErrors,
+				'loanData'   => $loanData,
 			]
 		);
 	}
