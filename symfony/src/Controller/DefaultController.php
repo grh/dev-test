05e1153e49f1b9bc8bb623fa5a3ed1aac8734e46
Aaron Smith <aaron@smartpayrentals.com>
2021-08-31 11:46:26 -0400

    Add form for user submission

--- symfony/src/Controller/DefaultController.php
+++ symfony/src/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\LoanType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,14 +17,23 @@ class DefaultController extends AbstractController
 	/**
 	 * @Route("/", name="home")
 	 */
-	public function home(): Response
+	public function home(Request $request): Response
 	{
+		$form = $this->createForm(LoanType::class);
 		
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$data = $form->getData();
+			
+			//  Process submitted data
+			
+			
+		}
 		
 		return $this->render(
 			'index.html.twig',
 			[
-				
+				'form' => $form->createView(),
 			]
 		);
 	}
